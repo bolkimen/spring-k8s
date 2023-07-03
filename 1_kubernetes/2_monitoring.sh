d6c9f8bfc8b2ea11b45cdf7e77c36c3287a7c7ee
Sergey Dreval <myblackbox.sergey@gmail.com>
2023-07-04 01:46:38 +0300

    add prometheus deployment

--- 1_kubernetes/2_monitoring.sh
+++ 1_kubernetes/2_monitoring.sh
@@ -3,3 +3,8 @@
 envsubst < $SCRIPT_DIR/monitoring/prometheus-cluster-role.yaml | kubectl apply -f -
 envsubst < $SCRIPT_DIR/monitoring/prometheus-pv-pvc.yaml | kubectl apply -f -
 
+
+envsubst < $SCRIPT_DIR/monitoring/prometheus-config-map.yaml | kubectl create -f -
+
+envsubst < $SCRIPT_DIR/monitoring/prometheus-deployment.yaml | kubectl apply -f -
+
