2edc4a46a31bfe39a4fbac707f61ea6a524e4421
Sergey <myblackbox.sergey@gmail.com>
2025-10-24 00:39:03 +0200

    add service discovery

---
+++ spring_cloud/service_config/src/main/java/com/bolkimen/microservice/service_config/ServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bolkimen.microservice.service_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigApplication.class, args);
+	}
+
+}
---
+++ spring_cloud/service_gateway/src/test/java/com/bolkimen/microservice/service_gateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolkimen.microservice.service_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
