a313761b5233df577937db6f13d9d4e3c2b74b99
Sergey <myblackbox.sergey@gmail.com>
2025-11-16 03:01:58 +0100

    asd

--- terraform_playground/simple_aws/main.tf
+++ terraform_playground/simple_aws/main.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "instnode" {
   instance_type = "t2.micro"
   ami           = "ami-abc123"
+
+  provisioner "local-exec" {
+    command = "echo 'Hello World' >example.txt"
+  }
+
 }
