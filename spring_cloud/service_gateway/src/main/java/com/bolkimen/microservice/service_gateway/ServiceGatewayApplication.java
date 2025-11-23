780e0ee4864e80472433927609fce0cf58bf5161
Sergey <myblackbox.sergey@gmail.com>
2025-11-23 13:17:16 +0100

    asd

--- spring_cloud/service_gateway/src/main/java/com/bolkimen/microservice/service_gateway/ServiceGatewayApplication.java
+++ spring_cloud/service_gateway/src/main/java/com/bolkimen/microservice/service_gateway/ServiceGatewayApplication.java
@@ -2,12 +2,35 @@ package com.bolkimen.microservice.service_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/servicea/**")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://127.0.0.1:8090"))
+				.route(p -> p
+						.path("/api/serviceb/**")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://127.0.0.1:8091"))
+				.build();
 	}
 
 }
