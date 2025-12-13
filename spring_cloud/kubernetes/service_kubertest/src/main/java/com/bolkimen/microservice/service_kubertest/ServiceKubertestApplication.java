322be547f485a1a3a2a8eb425c0cd606889258df
Sergey <myblackbox.sergey@gmail.com>
2025-12-13 13:51:16 +0100

    asd

---
+++ spring_cloud/kubernetes/service_kubertest/src/main/java/com/bolkimen/microservice/service_kubertest/ServiceKubertestApplication.java
@@ -0,0 +1,13 @@
+package com.bolkimen.microservice.service_kubertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceKubertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceKubertestApplication.class, args);
+	}
+
+}
