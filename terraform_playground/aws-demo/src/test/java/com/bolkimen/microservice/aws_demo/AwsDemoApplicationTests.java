9759903494da6636506930091e5d279c91f536ea
Sergey <myblackbox.sergey@gmail.com>
2025-12-25 12:49:04 +0100

    asd

---
+++ terraform_playground/aws-demo/src/test/java/com/bolkimen/microservice/aws_demo/AwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolkimen.microservice.aws_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
