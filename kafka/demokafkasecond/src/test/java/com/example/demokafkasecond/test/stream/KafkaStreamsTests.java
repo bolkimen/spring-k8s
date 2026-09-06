e4e7305bad417cc5b935bfeb9480d1f2dc339ecf
Sergey <386599+bolkimen@users.noreply.github.com>
2026-09-07 00:19:42 +0200

    asd

--- kafka/demokafkasecond/src/test/java/com/example/demokafkasecond/test/stream/KafkaStreamsTests.java
+++ kafka/demokafkasecond/src/test/java/com/example/demokafkasecond/test/stream/KafkaStreamsTests.java
@@ -1,10 +1,14 @@
 package com.example.demokafkasecond.test.stream;
 
+import com.example.demokafkasecond.service.WordCountProcessor;
 import com.example.demokafkasecond.util.KafkaTestsConstants;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,11 +27,12 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig
-@DirtiesContext
+//@DirtiesContext
 @EmbeddedKafka(partitions = 1,
         topics = {
                 KafkaTestsConstants.STREAMING_TOPIC1,
@@ -36,31 +41,34 @@ public class KafkaStreamsTests {
     @Autowired
     private EmbeddedKafkaBroker embeddedKafka;
 
+    //@Autowired
+    //private WordCountProcessor wordCountProcessor;
+
+    private WordCountProcessor wordCountProcessor = new WordCountProcessor();
+
     @Test
-    void someTest() {
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(this.embeddedKafka, "testGroup", true);
-        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        ConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
-        Consumer<Integer, String> consumer = cf.createConsumer();
-        this.embeddedKafka.consumeFromAnEmbeddedTopic(consumer, KafkaTestsConstants.STREAMING_TOPIC2);
-        ConsumerRecords<Integer, String> replies = KafkaTestUtils.getRecords(consumer);
-        assertThat(replies.count()).isGreaterThanOrEqualTo(1);
+    void givenInputMessages_whenProcessed_thenWordCountIsProduced() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        wordCountProcessor.buildPipeline(streamsBuilder);
+        Topology topology = streamsBuilder.build();
+
+        try (TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, new Properties())) {
+            TestInputTopic<String, String> inputTopic = topologyTestDriver
+                    .createInputTopic("input-topic", new StringSerializer(), new StringSerializer());
+
+            TestOutputTopic<String, Long> outputTopic = topologyTestDriver
+                    .createOutputTopic("output-topic", new StringDeserializer(), new LongDeserializer());
+
+            inputTopic.pipeInput("key", "hello world");
+            inputTopic.pipeInput("key2", "hello");
+
+            assertThat(outputTopic.readKeyValuesToList())
+                    .containsExactly(
+                            KeyValue.pair("hello", 1L),
+                            KeyValue.pair("world", 1L),
+                            KeyValue.pair("hello", 2L)
+                    );
+        }
     }
 
-    /*@Configuration
-    @EnableKafkaStreams
-    public static class TestKafkaStreamsConfiguration {
-
-        @Value("${" + EmbeddedKafkaBroker.SPRING_EMBEDDED_KAFKA_BROKERS + "}")
-        private String brokerAddresses;
-
-        @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-        public KafkaStreamsConfiguration kStreamsConfigs() {
-            Map<String, Object> props = new HashMap<>();
-            props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testStreams");
-            props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, this.brokerAddresses);
-            return new KafkaStreamsConfiguration(props);
-        }
-
-    }*/
 }
